d606445525329775421459219fe5216eb5834510
Rob Martin <robmartin@arista.com>
2022-07-14 11:55:46 -0500

    first commit

---
+++ avd_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+podman run --name rlab-avd -it -d \
+    -e AVD_MODE=demo \
+    -e AVD_GIT_USER="$(git config --get user.name)" \
+    -e AVD_GIT_EMAIL="$(git config --get user.email)" \
+    -v ./:/projects
+    -p 8080:8080 \
+    avdteam/vscode:latest
