262b1b9f3d3c4e17ea351d565c70cb4053f1b280
Rob Martin <rmartin@CMP-01.networkrob.com>
2022-07-14 12:01:16 -0500

    fixed typo

--- avd_run.sh
+++ avd_run.sh
@@ -4,6 +4,6 @@ podman run --name rlab-avd -it -d \
     -e AVD_MODE=demo \
     -e AVD_GIT_USER="$(git config --get user.name)" \
     -e AVD_GIT_EMAIL="$(git config --get user.email)" \
-    -v ./:/projects
+    -v ./:/projects \
     -p 8080:8080 \
     avdteam/vscode:latest
