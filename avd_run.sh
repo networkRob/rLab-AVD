7dd35ebace11022d413b15b24eedb6e1576285f3
Rob Martin <rmartin@CMP-01.networkrob.com>
2022-07-14 14:04:46 -0500

    updated bash run

--- avd_run.sh
+++ avd_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-podman run --name rlab-avd -it -d \
+sudo podman run --name rlab-avd -it -d \
     -e AVD_MODE=demo \
     -e AVD_GIT_USER="$(git config --get user.name)" \
     -e AVD_GIT_EMAIL="$(git config --get user.email)" \
-    -v ./:/projects \
+    -v ${PWD}:/home/avd/arista-ansible:Z \
     -p 8080:8080 \
     avdteam/vscode:latest
