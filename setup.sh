c770d12019ee078f0cfb18bf8c0d1d12f9aaa260
Rob Martin <rmartin@CMP-01.networkrob.com>
2022-07-15 14:56:55 -0500

    updated testing

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ansible-galaxy collection install arista.avd
+ansible-galaxy collection install arista.avd community.general
 
 pip3 install -r /home/avd/arista-ansible/ansible-avd/ansible_collections/arista/avd/requirements.txt
